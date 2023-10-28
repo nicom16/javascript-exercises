e809cc2dcb167c9089b2526247fea60b952f6a4f
Nicolò <nico.md.1600@gmail.com>
2023-10-28 13:48:59 +0200

    Added new exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,47 @@
-const add = function() {
-	
+const add = function(a, b) {
+  let result = a + b; 
+
+	return result;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  let result = a - b;
+
+  return result;
 };
 
-const sum = function() {
-	
+const sum = function(addends) {
+  let result = addends.reduce((sum, addend) => sum + parseInt(addend), 0);
+
+  return result;
 };
 
-const multiply = function() {
+const multiply = function(params) {
+  let result = 1;
 
+  params.forEach((param) => {
+    result = result * parseInt(param);
+  });
+
+  return result;
 };
 
-const power = function() {
-	
+const power = function(a, power) {
+  let result = a ** power;
+
+	return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  let result = 1;
+
+  if (x != 0 && x != 1) {
+    for (x; x > 0; x--) {
+      result = result * x; 
+    }
+  }
+
+	return result;
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,35 @@
-const palindromes = function () {
+const palindromes = function (word) {
+  let backward = "";
 
-};
+  for (let i = word.length - 1; i >= 0; i--) {
+    backward = backward + word[i];
+  } 
+
+  word = word.deleteSpacesAndPunctuation().toLowerCase();
+  backward = backward.deleteSpacesAndPunctuation().toLowerCase();
+
+  let isPalindrome = (word === backward) ? true : false;
+
+  return isPalindrome;
+}
+
+// Outputs the string without spaces and punctuation
+String.prototype.deleteSpacesAndPunctuation = function() {
+  let reducedWord = "";
+
+  for (let i = this.length - 1; i >= 0; i--) {
+    let character = this[i];
+    
+    let isPunctuation = !!character.match(/[.,:!?]/);
+    let isSpace = (character === ' ') ? true : false;
+
+    if (!isPunctuation && !isSpace) {
+      reducedWord = reducedWord + character;
+    }
+  }
+
+  return reducedWord;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
