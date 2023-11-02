6e3538ef255ed4ee63875bfeb49f5b0bc45063f4
Nicolò <nico.md.1600@gmail.com>
2023-11-02 14:42:53 +0100

    Added exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(position) {
+  if (position <= 0) return "OOPS";
+  position = parseInt(position);
 
+  let fibonaccisSequence = [0, 1];
+
+  for (let i = 2; i <= position; i++) {
+    fibonaccisSequence.push((fibonaccisSequence[i - 1] + fibonaccisSequence[i - 2]));
+  }
+
+  return fibonaccisSequence[position];
 };
 
 // Do not edit below this line
